4873bf4d367f2803fef0fbbe69df61f05a55672e
Barthélémy Ledoux <bledoux@kestra.io>
2025-06-06 09:54:50 +0200

    chore: upgrade storybook (#9326)

--- ui/.storybook/main.ts
+++ ui/.storybook/main.ts
@@ -1,25 +1,25 @@
 import type {StorybookConfig} from "@storybook/vue3-vite";
 
 const config: StorybookConfig = {
-  stories: [
-    "../tests/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-],
-  addons: [
-    "@storybook/addon-essentials",
-    "@storybook/addon-themes",
-    "@storybook/experimental-addon-test"
-  ],
-  framework: {
-    name: "@storybook/vue3-vite",
-    options: {},
-  },
-  async viteFinal(config) {
-    const {default: viteJSXPlugin} = await import("@vitejs/plugin-vue-jsx")
-    config.plugins = [
-      ...(config.plugins ?? []),
-      viteJSXPlugin(),
-    ];
-    return config;
-  },
+    stories: [
+        "../tests/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+    ],
+    addons: [
+        "@storybook/addon-themes",
+        "@storybook/addon-vitest",
+        "@storybook/addon-docs"
+    ],
+    framework: {
+        name: "@storybook/vue3-vite",
+        options: {},
+    },
+    async viteFinal(config) {
+        const {default: viteJSXPlugin} = await import("@vitejs/plugin-vue-jsx")
+        config.plugins = [
+            ...(config.plugins ?? []),
+            viteJSXPlugin(),
+        ];
+        return config;
+    },
 };
 export default config;
