436bef3baa7e9a9a2bfe2861dd91be8f70cf2e65
Bart Ledoux <bledoux@kestra.io>
2025-07-10 17:37:19 +0200

    Add the tabs

--- ui/src/components/flows/FlowPlayground.vue
+++ ui/src/components/flows/FlowPlayground.vue
@@ -1,17 +1,72 @@
 <template>
     <div class="playground">
-        Playground
+        <div class="pillTabs">
+            <button
+                v-for="tab in tabs"
+                :key="tab.name"
+                type="button"
+                :class="[{activeTab: tab.name === activeTab}, tab.name]"
+                @click="activeTab = tab.name"
+            >
+                {{ tab.label }}
+            </button>
+        </div>
+        <div class="content">
+            {{ activeTab }}
+        </div>
     </div>
 </template>
 
+<script setup lang="ts">
+    import {ref} from "vue";
+
+    const tabs = [{
+                      name: "logs",
+                      label: "Logs",
+                  },{
+                      name: "gantt",
+                      label: "Gantt",
+                  },
+                  {
+                      name: "outputs",
+                      label: "Outputs",
+                  },
+                  {
+                      name: "metrics",
+                      label: "Metrics",
+                  }
+    ]
+
+    const activeTab = ref("logs");
+</script>
+
 <style lang="scss" scoped>
+    @import "@kestra-io/ui-libs/src/scss/_color-palette";
+
     .playground {
-        display: flex;
-        align-items: center;
-        justify-content: center;
         height: 100%;
-        font-size: 1.5rem;
         color: var(--ks-color-text-secondary);
         background-color: var(--ks-background-panel);
+    }
+
+    .pillTabs {
+        display: inline-flex;
+        padding: 4px;
+        background-color:var(--ks-background-card) ;
+        margin: 1rem;
+        border-radius: 6px;
+        gap: 2px;
+        button{
+            padding: 0.2rem .5rem;
+            font-size: 14px;
+            color: var(--ks-content-tertiary);
+            background-color: transparent;
+            border: none;
+            border-radius: 4px;
+            &.activeTab {
+                color: var(--ks-content-primary);
+                background-color: $base-blue-500;
+            }
+        }
     }
 </style>
