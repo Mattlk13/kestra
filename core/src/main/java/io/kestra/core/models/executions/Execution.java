177ba35e15e4ceccd4708492df3d5f2a4d4dad9a
yuri <1969yuri1969@gmail.com>
2025-07-28 09:38:38 +0200

    fix(core): amend misc label-related issues (#10044)
    * fix(core): amend misc label-related issues
    
    * re-enabled bulk update of label value
    * re-enabled merging flow-execution labels by key
    * made duplicated keys rejection readable
    * forced multiple validations within `RequestUtils`
    * ensured existing labels can be overriden
    * added multiple tests validating complex scenarios
    
    BREAKING CHANGE: switched from first to last label value override
    BREAKING CHANGE: preventing empty key/value labels
    BREAKING CHANGE: preventing whitespace in key
    
    * fix(core): reflect feedback
    
    * Deduplicated a list inside the `Labels` task.
    * Worked around label mutation at `Worker`.
    * Attempted to deduplicate labels within `Execution` as possible.
    
    * fix(core): remove irrelevant changes

--- core/src/main/java/io/kestra/core/models/executions/Execution.java
+++ core/src/main/java/io/kestra/core/models/executions/Execution.java
@@ -24,6 +24,7 @@ import io.kestra.core.serializers.ListOrMapOfLabelSerializer;
 import io.kestra.core.services.LabelService;
 import io.kestra.core.test.flow.TaskFixture;
 import io.kestra.core.utils.IdUtils;
+import io.kestra.core.utils.ListUtils;
 import io.kestra.core.utils.MapUtils;
 import io.swagger.v3.oas.annotations.Hidden;
 import jakarta.annotation.Nullable;
@@ -132,7 +133,7 @@ public class Execution implements DeletedInterface, TenantInterface {
     }
 
     public List<Label> getLabels() {
-        return Optional.ofNullable(this.labels).orElse(new ArrayList<>());
+        return ListUtils.emptyOnNull(this.labels);
     }
 
     /**
@@ -177,7 +178,21 @@ public class Execution implements DeletedInterface, TenantInterface {
     }
 
 
+    /**
+     * Customization of Lombok-generated builder.
+     */
     public static class ExecutionBuilder {
+
+        /**
+         * Enforce unique values of {@link Label} when using the builder.
+         *
+         * @param labels The labels.
+         * @return Deduplicated labels.
+         */
+        public ExecutionBuilder labels(List<Label> labels) {
+            this.labels = Label.deduplicate(labels);
+            return this;
+        }
 
         void prebuild() {
             this.originalId = this.id;
@@ -226,7 +241,6 @@ public class Execution implements DeletedInterface, TenantInterface {
     }
 
     public Execution withLabels(List<Label> labels) {
-
         return new Execution(
             this.tenantId,
             this.id,
@@ -236,7 +250,7 @@ public class Execution implements DeletedInterface, TenantInterface {
             this.taskRunList,
             this.inputs,
             this.outputs,
-            labels,
+            Label.deduplicate(labels),
             this.variables,
             this.state,
             this.parentId,
@@ -366,7 +380,7 @@ public class Execution implements DeletedInterface, TenantInterface {
      *
      * @param resolvedTasks normal tasks
      * @param resolvedErrors errors tasks
-     * @param resolvedErrors finally tasks
+     * @param resolvedFinally finally tasks
      * @return the flow we need to follow
      */
     public List<ResolvedTask> findTaskDependingFlowState(
