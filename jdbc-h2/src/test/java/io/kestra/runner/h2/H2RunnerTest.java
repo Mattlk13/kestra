6a8ffbcac424b359b90d1d7893c0f3d4b87e4f17
nKwiatkowski <nkwiatkowski@kestra.io>
2025-09-23 15:55:14 +0200

    clean(tests): unwanted test

--- jdbc-h2/src/test/java/io/kestra/runner/h2/H2RunnerTest.java
+++ jdbc-h2/src/test/java/io/kestra/runner/h2/H2RunnerTest.java
@@ -1,14 +1,7 @@
 package io.kestra.runner.h2;
 
-import io.kestra.core.junit.annotations.LoadFlows;
 import io.kestra.jdbc.runner.JdbcRunnerTest;
-import org.junit.jupiter.api.Test;
 
 public class H2RunnerTest extends JdbcRunnerTest {
 
-    @Test
-    @LoadFlows({"flows/valids/restart-with-after-execution.yaml"})
-    protected void restartFailedWitertghAfterExecution() throws Exception {
-        restartCaseTest.restartFailedWithAfterExecution();
-    }
 }
